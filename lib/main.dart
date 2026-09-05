db68ed5270220f07d6eeec514c983b2b1c9526a9
Seif El Saeed <115191050+seif-007@users.noreply.github.com>
2026-09-06 02:57:56 +0300

    fixing the errors

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: SplashPage()
+    return MaterialApp(home: Home());
   }
 }
